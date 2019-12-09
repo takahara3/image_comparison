4a8e50d48b9b0b3642ca4558be1a7fcd86b4a7ea
takahara <kantz231017@gmail.com>
2019-12-09 19:54:52 +0900

    Changing parameters

--- classes.py
+++ classes.py
@@ -3,14 +3,14 @@ import cv2
 from matplotlib import pyplot as plt
 
 
-class Param:
+class Rect:
     def __init__(self, org_img, com_img):
         self.org_height = org_img.shape[0]
         self.org_width = org_img.shape[1]
         self.com_height = com_img.shape[0]
         self.com_width = com_img.shape[1]
-        self.sep_num = 3
-        self.slice_num = 3
+        self.sep_num = 4
+        self.slice_num = 10
         self.c_h = self.org_height // self.sep_num
         self.c_w = self.org_width // self.sep_num
         self.dh = self.c_h // self.slice_num
