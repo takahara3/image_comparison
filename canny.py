b1cae2aa1df5940eacfe12dfcd2f9940eb0c2f72
takahara <kantz231017@gmail.com>
2019-12-17 15:45:06 +0900

    fix code

--- canny.py
+++ canny.py
@@ -1,45 +1,55 @@
 import numpy as np
 import cv2
+import sys
 from matplotlib import pyplot as plt
 
 from functions import *
 
 if __name__ == "__main__":
+    args = get_option()
+
+    ###比較画像の読み込み
     org_img_path = "img/origin_image.png"
     com_img_path = "img/comparison_image.png"
-    org_img, com_img = Read_img(org_img_path, com_img_path)
+    org_img, com_img = read_img(org_img_path, com_img_path)
 
+    ###比較するために比較元画像を射影変換
+    org_img = image_conversion(org_img, com_img)
+
+    ###特徴点検出のためにGRAYスケール化
     gray_org_img = cv2.cvtColor(org_img, cv2.COLOR_BGR2GRAY)
     gray_com_img = cv2.cvtColor(com_img, cv2.COLOR_BGR2GRAY)
-    two_images_show(gray_org_img, gray_com_img)
+    #two_images_show(gray_org_img, gray_com_img)
 
+    ###Canny法によるエッジ検出
     min_val = 100
     max_val = 200
     org_edge_img = cv2.Canny(gray_org_img, min_val, max_val)
     com_edge_img = cv2.Canny(gray_com_img, min_val, max_val)
 
-    #特徴点の画素のRGB値
+    ###特徴点の画素のRGB値
     color = [255,255,255]
-    #判定に使う特徴点の差
+    ###差分判定に使う特徴点の差
     diff = 300
 
-    #矩形ごとの特徴点を比較，差分の大きい矩形の座標＆各矩形の登頂点数を取得
-    shape, org_point_num, com_point_num = Comparison(org_edge_img, com_edge_img, color, diff, True)
-    print(len(shape))
+    ###矩形ごとの特徴点を比較，差分の大きい矩形の座標＆各矩形の特徴点の数を取得
+    shape, org_point_num, com_point_num = Comparison(org_edge_img, com_edge_img, color, diff)
 
-    #全ての矩形の共通部分の矩形を取得
+    ###全ての矩形の共通部分の矩形を求める
     x = seek_intersect(shape)
-    print(x)
 
     img_1 = com_img.copy()
     img_2 = com_img.copy()
-    print(type(img_1))
 
-    #矩形を描画
+    ###矩形を描画
     cover_img = draw_common_rect(img_1, x)
-    after_img = draw_rect(img_2, shape)
 
-    two_images_show(after_img, cover_img)
+    image_show(cover_img)
 
-    #グラフの作成
-    #draw_graph(org_point_num, com_point_num)
+    ###グラフの作成
+    if args.figure == True:
+        draw_graph(org_point_num, com_point_num)
+
+    ###検出結果の保存
+    if args.save == True:
+        write_img(cover_img)
