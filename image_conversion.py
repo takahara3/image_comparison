aed23b78f5b246851f88637da352dc21967ed3b8
takahara <kantz231017@gmail.com>
2019-12-17 15:45:20 +0900

    fix code

--- image_conversion.py
+++ image_conversion.py
@@ -5,55 +5,6 @@ from matplotlib import pyplot as plt
 from functions import *
 from classes import *
 
-def keypoint_coords(kp, top, left, bottom, right, margin=0):
-    u = kp.pt[0]
-    v = kp.pt[1]
-    return (top + margin < v) and (left + margin < u) and (v < bottom - margin) and (u < right - margin)
-
-def image_conversion(org_img,com_img):
-    org_h, org_w, org_ch = org_img.shape
-    com_h, com_w, com_ch = com_img.shape
-
-    if org_h!=com_h or org_w!=com_w or org_ch!=com_ch:
-        print("Not equal size")
-    else:
-        gray_org_img = cv2.cvtColor(org_img, cv2.COLOR_RGB2GRAY)
-        gray_com_img = cv2.cvtColor(com_img, cv2.COLOR_RGB2GRAY)
-
-        detector = cv2.AKAZE_create()
-        org_kp, org_des = detector.detectAndCompute(gray_org_img, None)
-        com_kp, com_des = detector.detectAndCompute(gray_com_img, None)
-
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING)
-
-        matches = bf.match(org_des, com_des)
-        matches = sorted(matches, key = lambda x:x.distance)
-
-        margin = 50
-        matches = [match for match in matches \
-                   if keypoint_coords(org_kp[match.queryIdx], 0, 0, org_h, org_w, margin) \
-                   and keypoint_coords(com_kp[match.trainIdx], 0, 0, org_h, org_w, margin)]
-        num_correpondences = len(matches)
-        #print("Num of correspondences (filtered by image coordinates): {}".format(num_correpondences))
-
-        filtered_matches = matches[:100]
-
-        out = np.zeros((org_h, org_w*2, org_ch), np.uint8)
-        cv2.drawMatches(gray_org_img, org_kp, gray_com_img, com_kp, filtered_matches, out, flags=0)
-        out = cv2.cvtColor(out, cv2.COLOR_BGR2RGB)
-
-        good_match_rate = 0.7
-        good = matches[:int(len(matches) * good_match_rate)]
-        src_org_pts = np.float32([ org_kp[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-        dst_com_pts = np.float32([ com_kp[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
-
-        H, mask = cv2.findHomography(src_org_pts, dst_com_pts, cv2.RANSAC, 5.0)
-
-        warped_org_img = cv2.warpPerspective(org_img, H, (org_w, org_h))
-
-        return warped_org_img
-
-
 if __name__ == "__main__":
     org_img_path = "img/origin_image.png"
     com_img_path = "img/comparison_image.png"
