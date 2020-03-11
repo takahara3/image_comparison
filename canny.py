bb052e6a8930243162b7ca9c143c3507bc1434af
takahara <kantz231017@gmail.com>
2020-03-11 23:25:09 +0900

    fix code

--- canny.py
+++ canny.py
@@ -12,12 +12,20 @@ if __name__ == "__main__":
     args = get_option()
 
     ###比較画像の読み込み
-    org_img_path = args.image_1
-    com_img_path = args.image_2
+    org_img_path = 'img/' + args.image_1
+    com_img_path = 'img/' + args.image_2
     org_img, com_img = read_img(org_img_path, com_img_path)
+    two_images_show(org_img, com_img)
 
     ###比較するために比較元画像を射影変換
+    org_img_before = org_img
     org_img = image_conversion(org_img, com_img)
+    #two_images_show(org_img_before, org_img)
+
+    org_sample_path = 'img/org_grid.png'
+    com_sample_path = 'img/com_grid.png'
+    org_sample, com_sample =  read_img(org_sample_path, com_sample_path)
+    #two_images_show(org_sample, com_sample)
 
     ###特徴点検出のためにGRAYスケール化
     gray_org_img = cv2.cvtColor(org_img, cv2.COLOR_BGR2GRAY)
@@ -28,12 +36,19 @@ if __name__ == "__main__":
     max_val = 200
     org_edge_img = cv2.Canny(gray_org_img, min_val, max_val)
     com_edge_img = cv2.Canny(gray_com_img, min_val, max_val)
+    org_edge_img_show = cv2.cvtColor(org_edge_img, cv2.COLOR_RGB2BGR)
+    com_edge_img_show = cv2.cvtColor(com_edge_img, cv2.COLOR_RGB2BGR)
+    #two_images_show(org_edge_img_show, com_edge_img_show)
+
+    result_img_show = cv2.imread('./out/Canny_result.png')
+    result_img_show = cv2.cvtColor(result_img_show, cv2.COLOR_RGB2BGR)
+    #two_images_show(org_img_before, result_img_show)
 
     ###特徴点の画素のRGB値
     color = [255,255,255]
 
     ###差分判定に使う特徴点の差
-    ###特徴点の差が300個以上の矩形を差分とする
+    ###特徴点の差が250個以上の矩形を差分とする
     diff = 300
 
     ###矩形ごとの特徴点を比較，差分の大きい矩形の座標＆各矩形の特徴点の数を取得
@@ -48,6 +63,7 @@ if __name__ == "__main__":
     #処理時間を表示
     end_time = time.time() - start_time
     print('elapsed time = {}'.format(end_time))
+    two_images_show(img_1, img_2)
 
     ###矩形を描画
     if not shape:
@@ -55,15 +71,14 @@ if __name__ == "__main__":
     else:
         #result_img = draw_common_rect(img_1, common_rect)
         result_img = draw_rect(img_1, shape)
-        image_show(result_img)
+        #image_show(result_img)
+        ###検出結果の保存
+        if args.save == True:
+            if args.name != None:
+                write_img(result_img, img_name = args.name)
+            else:
+                write_img(result_img)
 
     ###グラフの作成
     if args.figure == True:
         draw_graph(org_point_num, com_point_num)
-
-    ###検出結果の保存
-    if args.save == True:
-        if args.name != None:
-            write_img(result_img, img_name = args.name)
-        else:
-            write_img(result_img)
